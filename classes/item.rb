73a2b13c83f0b61886f83accdad176061fc2201e
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-10-24 22:44:29 +0100

    Add attr_reader for id, archived, and genre

--- classes/item.rb
+++ classes/item.rb
@@ -1,7 +1,8 @@
 require 'date'
 
 class Item
-  attr_accessor :id, :author, :label, :publish_date, :genre
+  attr_accessor :author, :label, :publish_date
+  attr_reader :id, :archived, :genre
 
   def initialize(publish_date)
     @id = Random.rand(1..1000)
