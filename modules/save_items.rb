12435be7c8745b13e33f2143812f195d4ebac8bf
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-10-25 12:36:03 +0100

    Implement save_game method for saving games to JSON file

--- modules/save_items.rb
+++ modules/save_items.rb
@@ -1,5 +1,6 @@
 require 'json'
 require './classes/book/book'
+require './classes/game/game'
 
 module SaveItems
   def save_book
@@ -9,9 +10,21 @@ module SaveItems
           'publish_date' => book.publish_date,
           'publisher' => book.publisher,
           'cover_state' => book.cover_state,
-          'genre_id' => book.genre.id,
           'author_id' => book.author.id,
           'label_id' => book.label.id }
+      }.to_json
+    end
+  end
+
+  def save_game
+    File.open('./data/game.json', 'w') do |file|
+      file.puts @games.map { |game|
+        { 'id' => game.id,
+          'publish_date' => game.publish_date,
+          'multiplayer' => game.multiplayer,
+          'last_played_at' => game.last_played_at,
+          'label_id' => game.label.id,
+          'author_id' => game.author.id }
       }.to_json
     end
   end
