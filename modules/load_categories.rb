7cbfd2d0cdd30147122f26dbbf93a106456e42aa
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-10-25 12:33:27 +0100

    Implement load_authors method for loading authors from JSON file

--- modules/load_categories.rb
+++ modules/load_categories.rb
@@ -1,5 +1,6 @@
 require 'json'
 require './classes/label/label'
+require './classes/author/author'
 
 module LoadCategories
   def load_labels
@@ -9,6 +10,16 @@ module LoadCategories
     labels_hash = JSON.parse(file)
     labels_hash.each do |label|
       @labels << Label.new(label['title'], label['color'], label['id'])
+    end
+  end
+
+  def load_authors
+    return unless File.exist?('data/authors.json')
+
+    file = File.read('data/authors.json')
+    authors_hash = JSON.parse(file)
+    authors_hash.each do |author|
+      @authors << Author.new(author['first_name'], author['last_name'], author['id'])
     end
   end
 end
